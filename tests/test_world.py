a27ec0c029fed8a15e5df1f3d41ae3f2ba61ac62
sparrow0hawk <alexjcoleman@gmail.com>
2020-09-13 16:21:57 +0100

    added get_name to world.py and more tests

--- tests/test_world.py
+++ tests/test_world.py
@@ -10,3 +10,14 @@ class testWorldClass(unittest.TestCase):
     def test_limits(self) -> None:
 
         self.assertEqual(self.test_world.get_limits(), (100,100))
+
+
+    def test_get_name(self) -> None:
+
+        self.assertTrue(isinstance(self.test_world.get_name(), str))
+
+    def test_get_name_unique(self) -> None:
+        
+        world_set = set([World(coords = (10,10)).get_name() for _ in range(1000)])
+
+        self.assertEqual(len(world_set), 1000)
--- walkers/world.py
+++ walkers/world.py
@@ -5,9 +5,12 @@ class World:
     new_name = itertools.count()
 
     def __init__(self, coords : tuple) -> None:
-        self.name = "word_" + str(next(self.new_name))
+        self.name = "world_" + str(next(self.new_name))
         self.xlimit = coords[0]
         self.ylimit = coords[1]
 
     def get_limits(self):
         return (self.xlimit, self.ylimit)
+
+    def get_name(self):
+        return self.name
