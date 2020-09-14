a7e24e277192e910c7a3649498320a16c44d6d33
sparrow0hawk <alexjcoleman@gmail.com>
2020-09-14 12:11:59 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Walkers',
+    version='0.1dev',
+    packages=['walkers',],
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ]
+)
